744e0ff543116605d20130e505c8836a8f89fc8b
Seapolco <seapolco@gmail.com>
2022-10-17 17:16:40 +0100

    first commit. basic webpack complete.

---
+++ dist/todoBundle0fab2bd1ea9ad0fb9959.js
@@ -0,0 +1,9 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+console.log('just todo it!');
+/******/ })()
+;
+//# sourceMappingURL=todoBundle0fab2bd1ea9ad0fb9959.js.map
---
+++ src/index.js
@@ -0,0 +1,1 @@
+console.log('just todo it!')
