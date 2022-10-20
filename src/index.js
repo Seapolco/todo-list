7d24bce1256ccb77f314d44bc0a72783f9793e38
Seapolco <seapolco@gmail.com>
2022-10-20 14:03:40 +0100

    Added some basic date functionality
    Installed date-fns, extremely minimal grasp of it atm.
    Added date select when adding todo
    Created a function to format the date, displaying the day.
    Displayed on todo list

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import './main.css'
 
+import { format } from 'date-fns'
+
 console.log('just todo it!')
 
 // create factory function for generating to-do
@@ -14,6 +16,30 @@ const addTodoBtn = document.querySelector('.addTodoButton')
 const displayTodos = document.querySelector('.displayTodos')
 const todoList = document.querySelector('.todoList')
 
+const dateSelect = document.querySelector('#dateSelect')
+
+console.log(dateSelect.value)
+
+let dateReturn = '2022 - 10 - 20'
+
+let today = new Date(2022, 9, 20)
+
+today = format(today, 'E-dd-MM-yyyy')
+
+function formatDate(date) {
+  let dateArr = date.split('-')
+  let year = Number(dateArr[0])
+  let month = Number(dateArr[1]) - 1
+  let day = Number(dateArr[2])
+
+  let newDate = new Date(year, month, day)
+  let formattedDate = format(newDate, 'E-dd-MM-yyyy')
+  //console.log(formattedDate)
+  return formattedDate
+}
+
+//console.log(today)
+
 console.log(todoList)
 
 console.log(todoForm)
@@ -22,10 +48,11 @@ let allTodos = []
 
 console.log(prioritySelect)
 
-function Todo(title, description, priority) {
+function Todo(title, description, priority, date) {
   this.title = title
   this.description = description
   this.priority = priority
+  this.date = date
 }
 
 function addTodo(todo) {
@@ -76,20 +103,31 @@ function createTodoListItem(todo) {
     { class: 'todoPriority' },
     'priority'
   )
+  let todoDate = elementFactory(todo, 'p', { class: 'todoDate' }, 'date')
 
   todoLi.appendChild(checkIcon)
   todoLi.appendChild(todoTitle)
   todoLi.appendChild(todoDescription)
   todoLi.appendChild(todoPriority)
+  todoLi.appendChild(todoDate)
 
   todoList.appendChild(todoLi)
 }
 
-let pullworkout = new Todo('Pull', 'Back, Biceps, Sqaut, Deadlift', 'High')
-let pushworkout = new Todo('Push', 'Chest, Triceps, Sqaut, Press', 'High')
+// let pullworkout = new Todo(
+//   'Pull',
+//   'Back, Biceps, Sqaut, Deadlift',
+//   'High',
+//   new Date()
+// )
+// let pushworkout = new Todo(
+//   'Push',
+//   'Chest, Triceps, Sqaut, Press',
+//   'High',
+// )
 
-addTodo(pullworkout)
-addTodo(pushworkout)
+// addTodo(pullworkout)
+// addTodo(pushworkout)
 
 // allTodos.forEach((e) => {
 //   createTodoListItem(e)
@@ -103,9 +141,14 @@ const addAllTodos = (todoArr) => {
 
 //addAllTodos(allTodos)
 
+addAllTodos(allTodos)
+
 //createTodoListItem(workout)
 
 newTodoBtn.addEventListener('click', (e) => {
+  console.log(dateSelect.value)
+
+  dateSelect.value = ''
   todoForm.style.display = 'grid'
   displayTodos.style.display = 'none'
 })
@@ -116,11 +159,15 @@ addTodoBtn.addEventListener('click', (e) => {
   let title = titleInput.value
   let description = descrTextArea.value
   let priority = prioritySelect.value
+  let date = formatDate(dateSelect.value)
 
   console.log('addddddd')
-  let todo = new Todo(title, description, priority)
+  let todo = new Todo(title, description, priority, date)
   addTodo(todo)
   console.log(allTodos)
+  console.log(dateSelect.value)
+  console.log('format', formatDate(dateSelect.value))
+  console.log(typeof dateSelect.value)
   clearForm()
   clearTodos()
   addAllTodos(allTodos)
